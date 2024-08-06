f29cc2efc68e2727c9dd61a4bede5dba06746cdf
drukowaniew3d@gmail.com <drukowaniew3d@gmail.com>
2024-08-06 20:29:52 +0200

    ready to tests

--- matcher_iterator.sh
+++ matcher_iterator.sh
@@ -4,13 +4,8 @@ if [ ! -d "third_party/SGMNet/weights" ]; then
     tar -xzf third_party/SGMNet/weights.tar.gz -C third_party/SGMNet
 fi
 
-# dataset_path="/path/to/dataset"
-dataset_path="/mnt/e/Czajka_dataset/20230927_1"
+dataset_path="/path/to/20230927_1"
 matchers_list=("LoFTR" "TopicFM" "AspanFormer" "DeDoDe" "SuperPoint+SuperGlue" "SuperPoint+LightGlue" "DISK" "DISK+dualsoftmax" "superpoint+dualsoftmax" "DISK+LightGlue" "SuperPoint+MNN" "SIFT+SGMNet" "SOSNet" "HardNet" "D2Net" "RORD" "ALIKE" "LANET" "R2D2" "DARKFeat" "SIFT" "ROMA" "DKMv3" "GlueStick" "SOLD2")
-
-# fail_cuda=("d2net" "rord" "lanet" "DKMv3")
-
-matchers_list=("aspanformer")
 
 for matcher in "${matchers_list[@]}"; do
     echo "$matcher"
